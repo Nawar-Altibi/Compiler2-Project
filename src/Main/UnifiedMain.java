a5743504ebab38b850dd502cfd4acff653554fa1
nawar <nawaraltibi46@gmail.com>
2026-06-03 15:38:58 +0300

    Fix syntax and structure errors in UnifiedMain.java

--- src/Main/UnifiedMain.java
+++ src/Main/UnifiedMain.java
@@ -5,6 +5,9 @@ import org.antlr.v4.runtime.tree.ParseTree;
 
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class UnifiedMain {
     public static void main(String[] args) {
@@ -23,8 +26,8 @@ public class UnifiedMain {
                 runFlaskCompiler(filePath);
             } else if (fileName.endsWith(".html")) {
                 System.out.println("Dispatcher: Detected HTML/CSS/Jinja file.");
-                runHtmlCssCompiler(filePath, new java.util.HashMap<>());
-            }    else {
+                runHtmlCssCompiler(filePath, new HashMap<>());
+            } else {
                 System.out.println("Unknown file type: " + fileName);
                 System.out.println("Supported extensions: .py, .html, .txt, .ts");
             }
@@ -45,41 +48,38 @@ public class UnifiedMain {
         compilers.flask.ast.builder.ASTBuilder builder = new compilers.flask.ast.builder.ASTBuilder();
         compilers.flask.ast.nodes.statements.ProgramNode ast = (compilers.flask.ast.nodes.statements.ProgramNode) builder.visit(tree);
 
-        System.out.println("====== Flask AST ======");
+        System.out.println("====== Flask SYMBOL TABLE BUILDING ======");
+        compilers.flask.SymbolTable.SymbolTableBuilder stBuilder = new compilers.flask.SymbolTable.SymbolTableBuilder();
+        ast.accept(stBuilder);
+        compilers.flask.SymbolTable.SymbolTable table = stBuilder.getSymbolTable();
+
+        System.out.println("====== Flask SEMANTIC ANALYSIS ======");
+        compilers.flask.semantic.SemanticAnalyzer semanticAnalyzer = new compilers.flask.semantic.SemanticAnalyzer(table, path);
+        semanticAnalyzer.analyze(ast);
+        
+        if (semanticAnalyzer.getErrorReporter().hasErrors()) {
+            semanticAnalyzer.getErrorReporter().printErrors();
+        } else {
+            System.out.println("No semantic errors found.");
+        }
+
+        System.out.println("\n====== Flask AST ======");
         compilers.flask.Visitor.ASTPrinter printer = new compilers.flask.Visitor.ASTPrinter();
         printer.visitProgram(ast);
         System.out.println(printer.getOutput());
 
         System.out.println("\n====== Flask SYMBOL TABLE ======");
-        compilers.flask.SymbolTable.SymbolTableBuilder stBuilder = new compilers.flask.SymbolTable.SymbolTableBuilder();
-        ast.accept(stBuilder);
-        compilers.flask.SymbolTable.SymbolTable table = stBuilder.getSymbolTable();
         table.print();
+
+        // Optional: Collect template contexts for cross-file validation
+        compilers.flask.semantic.TemplateContextCollector collector = new compilers.flask.semantic.TemplateContextCollector();
+        ast.accept(collector);
     }
 
-    private static void runHtmlCssCompiler(String path) throws Exception {
+    private static void runHtmlCssCompiler(String path, Map<String, Set<String>> templateContexts) throws Exception {
         CharStream cs = CharStreams.fromFileName(path);
         compilers.html_css.antlr.HtmlCssLexer lexer = new compilers.html_css.antlr.HtmlCssLexer(cs);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
-        compilers.html_css.antlr.HtmlCssParser parser = new compilers.html_css.antlr.HtmlCssParser(tokens);
-
-        ParseTree tree = parser.htmlDocument();
-
-        compilers.html_css.Visitor.HtmlAstBuilder builder = new compilers.html_css.Visitor.HtmlAstBuilder();
-        compilers.html_css.ast.HtmlNode ast = builder.visit(tree);
-
-        System.out.println("====== HTML/CSS AST ======");
-        compilers.html_css.Visitor.AstPrintVisitor printer = new compilers.html_css.Visitor.AstPrintVisitor();
-        ast.accept(printer);
-
-        System.out.println("\n====== HTML/CSS SYMBOL TABLE ======");
-        compilers.html_css.SymbolTable.SymbolTableBuilder stBuilder = new compilers.html_css.SymbolTable.SymbolTableBuilder();
-        compilers.html_css.SymbolTable.SymbolTable table = stBuilder.build(ast);
-        System.out.println(table.printSymbolTable());
-    }
-
-}
-  CommonTokenStream tokens = new CommonTokenStream(lexer);
         compilers.html_css.antlr.HtmlCssParser parser = new compilers.html_css.antlr.HtmlCssParser(tokens);
 
         ParseTree tree = parser.htmlDocument();
@@ -109,5 +109,4 @@ public class UnifiedMain {
         System.out.println("\n====== HTML/CSS SYMBOL TABLE ======");
         System.out.println(table.printSymbolTable());
     }
-
 }
